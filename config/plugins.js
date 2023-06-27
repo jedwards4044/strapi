566ff23d4e00334a9b704496a01491824de334bc
jedwards4044 <jedwards404@gmail.com>
2023-06-27 11:49:13 -0400

    feat: use strapi saved data to get images/buttons

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   "strapi-plugin-populate-deep": {
     config: {
-      defaultDepth: 23, // Default is 5
+      defaultDepth: 10, // Default is 5
     },
   },
   graphql: {
