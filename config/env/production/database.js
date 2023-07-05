9097bfbafbede17ea750604b8428d8c0b9cc19aa
jedwards4044 <jedwards404@gmail.com>
2023-07-04 22:14:03 -0400

    feat: add fly.io production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,15 +1,16 @@
-module.exports = ({ env }) => ({
+// ./config/env/production/database.js
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = () => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: false,
     },
     debug: false,
   },
