03861bdc9b9769268beb2f0e8a725ce23cacad73
jedwards4044 <jedwards404@gmail.com>
2023-07-28 11:40:25 -0400

    feat: navigation save changes (ngen-47)

--- config/plugins.js
+++ config/plugins.js
@@ -16,6 +16,23 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  navigation: {
+    enabled: true,
+    config: {
+      additionalFields: [
+        "audience",
+        { name: "my_custom_field", type: "boolean", label: "My custom field" },
+      ],
+      contentTypes: ["api::page.page"],
+      contentTypesNameFields: {
+        "api::page.page": ["title"],
+      },
+      pathDefaultFields: {
+        "api::page.page": ["slug"],
+      },
+      allowedLevels: 2,
+    },
+  },
 
   upload: {
     config: {
